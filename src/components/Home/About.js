359a465788cf9d74900752a68ec1d501f353df2b
jewelCode <jewelbarua101@gmail.com>
2022-06-10 16:07:51 +0600

    about section updated

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -4,16 +4,22 @@ import about from '../resources/about.png'
 
 const About = () => {
     return (
+        <div className="bg-light p-3">
         <div className="container mt-5">
             <h2 className="text-center fw-bold display-4"><span className="text-danger">About</span> Me</h2>
             <div className="row d-flex align-items-center">
                 <div className="col-md-6 d-flex justify-content-center text-center">
-                    <img className="img-fluid" src={about} alt="" />
+                    <div className="border rounded-circle bg-danger">
+                        <img className="img-fluid" src={about} alt="" />
+                    </div>
                 </div>
                 <div className="col-md-6 mt-5 d-flex justify-content-center">
                     <p className="fs-4">My Name is Jewel Barua. I am a front End Developer. I can make any Kind of web application you need</p>
                 </div>
             </div>
+        </div>
+        
+
         </div>
     );
 };
--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <div className="row d-flex align-items-center">
                 <div className="col-md-6">
                     <h1 className="display-1 fw-bold">Hello</h1>
-                    <h1 className="display-1 fw-bold">I'm <span className="text-danger">Jewel Barua</span></h1>
+                    <h1 className="display-1 fw-bold">I'm <span className="text-danger">Jewel</span> Barua</h1>
                     <p className="display-6">Front End Developer</p>
                     <br />
                     <Button variant="danger" size="lg" href="https://drive.google.com/file/d/1s4cQ_XILpeOUdoXczgpTQPK3TGp7zeEr/view?usp=sharing" target="_blank">Download Resume</Button>
